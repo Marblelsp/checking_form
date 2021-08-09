20477b5b56248e6b0687c9ef66d27fee26958ca3
marblelsp <marblelsp@LAPTOP-31H2I27V.localdomain>
2021-08-09 04:44:30 +0300

    first_commit

---
+++ todoform/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import ModelForm
+
+
+@admin.register(ModelForm)
+class ModelFormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date', 'text') 
+    empty_value_display = '-пусто-'
---
+++ todoform/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodoformConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todoform'
